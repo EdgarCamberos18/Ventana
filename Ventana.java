e35c8fde807ee66b39b579ac668f80259632375b
EdgarCamberos18 <edulcamberosar@ittepic.edu.mx>
2016-02-22 05:27:01 -0700

    Primer commit

---
+++ Ventana.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana{
+
+	public static void main (String ar[]){
+		JFrame ventana = new JFrame("Titulo");
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.setVisible(true);	
+	}
+
+}
