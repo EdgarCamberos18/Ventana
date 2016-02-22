7ba217ae86745ebe55db76a61abd8632c2531717
EdgarCamberos18 <edulcamberosar@ittepic.edu.mx>
2016-02-22 06:18:00 -0700

    Segundo commit

--- Ventana.java
+++ Ventana.java
@@ -6,7 +6,10 @@ public class Ventana{
 	public static void main (String ar[]){
 		JFrame ventana = new JFrame("Titulo");
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventana.setVisible(true);	
+		ventana.setVisible(true);
+
+		Container container = ventana.getContentPane();
+		container.setLayout(new GridBagLayout());	
 	}
 
 }
